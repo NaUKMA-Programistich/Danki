476bd7ca49d20c1e5e7abbdbef954b3ab4fa0342
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2023-10-25 15:14:33 +0300

    Define CardService interface

---
+++ backend/src/main/kotlin/ua/ukma/edu/danki/services/CardService.kt
@@ -0,0 +1,20 @@
+package ua.ukma.edu.danki.services
+
+import ua.ukma.edu.danki.models.CardDTO
+import ua.ukma.edu.danki.models.CardSortParam
+import java.util.UUID
+
+interface CardService {
+    suspend fun createCard(card: CardDTO, user: UUID)
+    suspend fun readCard(card: Long, user: UUID)
+    suspend fun deleteCards(card: List<CardDTO>, user: UUID)
+    suspend fun updateCard(card: CardDTO, user: UUID)
+    suspend fun readCollectionCards(
+        collection: UUID,
+        offset: Int,
+        limit: Int,
+        sort: CardSortParam,
+        ascending: Boolean,
+        user: UUID
+    )
+}
