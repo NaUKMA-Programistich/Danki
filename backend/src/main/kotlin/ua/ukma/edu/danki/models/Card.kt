c331df4dd041b015ffd1a52a766f04b7ada5f4ca
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2023-10-25 14:59:40 +0300

    add temporal columns to cards table

--- backend/src/main/kotlin/ua/ukma/edu/danki/models/Card.kt
+++ backend/src/main/kotlin/ua/ukma/edu/danki/models/Card.kt
@@ -4,11 +4,16 @@ import org.jetbrains.exposed.dao.LongEntity
 import org.jetbrains.exposed.dao.LongEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.LongIdTable
+import org.jetbrains.exposed.sql.kotlin.datetime.CurrentTimestamp
+import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
+import ua.ukma.edu.danki.models.CardCollections.defaultExpression
 
 object Cards : LongIdTable() {
     val term = varchar("term", length = 50)
     val definition = text("definition")
     val collection = reference("card-collection", CardCollections).nullable()
+    val lastModified = timestamp("last_modified").defaultExpression(CurrentTimestamp())
+    val timeAdded = timestamp("time_added").defaultExpression(CurrentTimestamp())
 }
 
 class Card(id: EntityID<Long>) : LongEntity(id) {
@@ -17,5 +22,6 @@ class Card(id: EntityID<Long>) : LongEntity(id) {
     var term by Cards.term
     var definition by Cards.definition
     var collection by CardCollection optionalReferencedOn Cards.collection
-
+    val timeAdded by Cards.timeAdded
+    var lastModified by Cards.lastModified
 }
