79afb68f9a5c43053e5c8c2c6703f0746a6b36dc
Andrii Dubovyk <and.dubovyk@gmail.com>
2023-10-28 16:53:42 +0300

    Add SearchEvent

---
+++ composeApp/src/commonMain/kotlin/ua/ukma/edu/danki/screens/search/viewmodel/SearchEvent.kt
@@ -0,0 +1,6 @@
+package ua.ukma.edu.danki.screens.search.viewmodel
+
+sealed class SearchEvent {
+    data class ChangeInput(val newInput: String): SearchEvent()
+    data class SelectWord(val word: String): SearchEvent()
+}
