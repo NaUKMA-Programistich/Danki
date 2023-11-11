f8894620177eb3279d49ac3f61890c8d21dd0858
Danylo Nechyporchuk <danylo.nechyporchuk@gmail.com>
2023-11-11 23:52:43 +0200

    add screen to navGraph

--- composeApp/src/commonMain/kotlin/ua/ukma/edu/danki/navigation/NavigationGraph.kt
+++ composeApp/src/commonMain/kotlin/ua/ukma/edu/danki/navigation/NavigationGraph.kt
@@ -2,6 +2,7 @@ package ua.ukma.edu.danki.navigation
 
 import ru.alexgladkov.odyssey.compose.extensions.screen
 import ru.alexgladkov.odyssey.compose.navigation.RootComposeBuilder
+import ua.ukma.edu.danki.screens.game.GameScreen
 import ua.ukma.edu.danki.screens.login.LoginScreen
 
 internal fun RootComposeBuilder.NavigationGraph() {
@@ -9,9 +10,11 @@ internal fun RootComposeBuilder.NavigationGraph() {
     screen(NavigationRoute.Login.name) {
         LoginScreen()
     }
-
+    screen(NavigationRoute.Login.name) {
+        GameScreen(collectionId = it as String)
+    }
 }
 
 internal enum class NavigationRoute {
-    Login,
+    Login, Game
 }
