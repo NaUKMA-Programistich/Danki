a3f939ca1f7a1dcefac565ad80010e8e8325ef62
Danylo Nechyporchuk <75624164+CorTexDotMe@users.noreply.github.com>
2023-11-14 17:37:45 +0200

    Game screen results (#17)
    * basically complete game screen
    
    * add screen to navGraph
    
    * fix screen in navGraph
    
    * add finish game top bar menu option
    
    * change showDefinition
    
    Now game can show definition on screen instead of redirecting to definition screen
    
    * make game screen content fit in 0-600 dp
    
    * almost complete game results screen
    
    * enable navigation
    
    * animate show definition
    
    Show card definition right beneath the card term. Remove previous implementation of redirecting to card definition screen.
    
    * small change
    
    Show definition with AnimatedVisibility. Use surface color for small view
    
    * add spacer

--- composeApp/src/commonMain/kotlin/ua/ukma/edu/danki/navigation/NavigationGraph.kt
+++ composeApp/src/commonMain/kotlin/ua/ukma/edu/danki/navigation/NavigationGraph.kt
@@ -2,25 +2,35 @@ package ua.ukma.edu.danki.navigation
 
 import ru.alexgladkov.odyssey.compose.extensions.screen
 import ru.alexgladkov.odyssey.compose.navigation.RootComposeBuilder
+import ua.ukma.edu.danki.models.CardDTO
+import ua.ukma.edu.danki.screens.game.GameScreen
+import ua.ukma.edu.danki.screens.game_results.GameResultsScreen
 import ua.ukma.edu.danki.screens.game.GameScreen
 import ua.ukma.edu.danki.screens.collections.CollectionsScreen
 import ua.ukma.edu.danki.screens.login.LoginScreen
 
 internal fun RootComposeBuilder.NavigationGraph() {
-
     screen(NavigationRoute.Login.name) {
         LoginScreen()
     }
-    
+
     screen(NavigationRoute.Collections.name) {
         CollectionsScreen()
     }
-    
+
     screen(NavigationRoute.Game.name) {
         GameScreen(collectionId = it as String)
+    }
+
+    screen(NavigationRoute.GameResults.name) { cardsAndResults ->
+        if (cardsAndResults !is Pair<*, *>) return@screen
+        GameResultsScreen(
+            cards = (cardsAndResults.first as List<CardDTO>),
+            gameResults = cardsAndResults.second as List<Boolean>
+        )
     }
 }
 
 internal enum class NavigationRoute {
-    Login, Game, Collections
+    Login, Collections, Game, GameResults
 }
