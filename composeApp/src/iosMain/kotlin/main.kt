9d20d3faa77af86522b5e6ccc1b0168ada6b9c3f
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2023-11-19 17:35:26 +0200

    Fix ios build

--- composeApp/src/iosMain/kotlin/main.kt
+++ composeApp/src/iosMain/kotlin/main.kt
@@ -1,7 +1,15 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import ua.ukma.edu.danki.App
 import platform.UIKit.UIViewController
+import ru.alexgladkov.odyssey.compose.setup.OdysseyConfiguration
+import ua.ukma.edu.danki.theme.mColors
 
 fun MainViewController(systemAppearance: (isLight: Boolean) -> Unit): UIViewController {
-    return ComposeUIViewController { App(systemAppearance) }
+    return ComposeUIViewController {
+        val configuration = OdysseyConfiguration(
+            backgroundColor = mColors.background,
+        )
+
+        App(systemAppearance, configuration)
+    }
 }
